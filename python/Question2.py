7d2f37fab90d57295d2970256d1e12a1943db0ca
ProtoManGG <52622543+ProtoManGG@users.noreply.github.com>
2019-10-05 10:38:04 +0530

    Update Question2.py

--- python/Question2.py
+++ python/Question2.py
@@ -1,5 +1,6 @@
 # modify this function, and create other functions below as you wish
 def question02(risk, bonus, trader):
-    # modify and then return the variable below
-    answer = -5
+    for i in range(0,len(trader)):
+        n.append(bonus[i]+trader[i]-risk[i])
+    answer = len(trader)*(bonus[n.index(max(n))])
     return answer
