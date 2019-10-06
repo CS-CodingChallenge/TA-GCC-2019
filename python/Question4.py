1e1183af929b8fb6d1c12a5324070aaffaeda112
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2019-10-06 20:38:34 +0530

    Update Question4.py

--- python/Question4.py
+++ python/Question4.py
@@ -1,5 +1,12 @@
 # modify this function, and create other functions below as you wish
 def question04(v, c, mc):
     # modify and then return the variable below
-    answer = -5
+    dp = [0]*(mc+1)
+    for i in range(len(v)):
+        
+        for j in range(mc,c[i]-1,-1):
+            
+            dp[j] = max(dp[j],v[i]+dp[j-c[i]])
+            
+    answer = dp[mc]
     return answer
