0b3e1252cf29cf3f079ce7fd10fdc3db1f98e05b
gurupk <pkhatri981@gmail.com>
2019-10-10 20:25:01 +0530

    Update Question2.py

--- python/Question2.py
+++ python/Question2.py
@@ -1,5 +1,13 @@
 # modify this function, and create other functions below as you wish
 def question02(risk, bonus, trader):
+    s=0
+    a=0
+    for skill in trader:
+        for i in range(len(risk)):
+            if risk[i] <= skill and a<bonus[i]:
+                a=bonus[i]
+        s+=a
+    
     # modify and then return the variable below
-    answer = -5
+    answer = s
     return answer
