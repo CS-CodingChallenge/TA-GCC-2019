52a430abc63a83b87e80079ea227818121372a67
Dhanam <42117184+Dhanam77@users.noreply.github.com>
2019-10-12 22:50:13 +0530

    Updated Q2

--- java/src/main/java/skeleton/answers/Question2.java
+++ java/src/main/java/skeleton/answers/Question2.java
@@ -3,7 +3,34 @@ package skeleton.answers;
 public class Question2 {
 
     public static int riskAndReward(int[] risk, int[] bonus, int[] trader) {
-        // TODO Auto-generated method stub
-        return -5;
+    	int arrayLength = trader.length;
+ 	   int maxBonus = 0, totalBonus = 0,maxIndex = 0, minRisk = 10000;
+ 		//Finding the maximum bonus in the bonus array
+ 		for(int i = 0; i < arrayLength; i++)
+ 		{
+ 			if(bonus[i] >= maxBonus)
+ 			{
+ 				maxBonus = bonus[i];
+ 				
+ 				//Checking the risk associated with maximum bonus
+ 				if(risk[i] < minRisk)
+ 				{
+ 					//Taking the index of minimum risk
+ 					minRisk = risk[i];
+ 					maxIndex = i;
+ 				}
+ 				
+ 			}
+ 		}
+ 		//Finding the total maximum bonus	
+ 		for(int j = 0; j < arrayLength; j++)
+ 		{
+ 			if(trader[j] >= risk[maxIndex])
+ 			{
+ 				totalBonus += maxBonus; 
+ 			}
+ 		}
+ 		return totalBonus;
+     
     }
 }
