2743d8caa565a3d1ee6cb171657d30a5a1f87ab6
SINDHUSITA <52288575+SINDHUSITA@users.noreply.github.com>
2019-10-13 17:54:33 +0530

    Update Question2.py

--- python/Question2.py
+++ python/Question2.py
@@ -1,5 +1,13 @@
 # modify this function, and create other functions below as you wish
 def question02(risk, bonus, trader):
     # modify and then return the variable below
-    answer = -5
+    answer=0
+    for i in trader:
+        l=list()
+        c=0
+        for j in risk:
+            if(i>=j):
+                l.append(bonus[c])
+            c+=1
+        answer=answer+max(l)        
     return answer
