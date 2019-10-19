a72c640eb4c9f53f65e5128bf6d48178cc40ef1d
ridh7 <46676917+ridh7@users.noreply.github.com>
2019-10-19 21:56:42 +0530

    Answer for Question1

--- java/src/main/java/skeleton/answers/Question1.java
+++ java/src/main/java/skeleton/answers/Question1.java
@@ -4,7 +4,21 @@ public class Question1 {
 
     public static double calculateTotalPayment(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage) {
         // TODO Auto-generated method stub
-        return -5;
+        double ild = initialLevelOfDebt;
+		double ip = interestPercentage;
+		double rp = repaymentPercentage;
+		double d = 0.1 * ild;
+		double r = (rp/100) * ild;
+		int c = 0;
+		while(ild > 50)
+		{
+			ild = (1 + ip/100) * ild;
+			ild -= r;
+			c++;
+			
+		}
+		double res = c * r + ild + d;
+        return res;
     }
 
 }
