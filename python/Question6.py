3b456a94f9f8e2d04bb9e873652b4c6ba32c8d9f
lucaschn <lschneidpro@gmail.com>
2019-10-19 15:05:29 +0200

    add2

--- python/Question6.py
+++ python/Question6.py
@@ -1,5 +1,14 @@
 # modify this function, and create other functions below as you wish
+import numpy as np
+
+
+portfolio=["HOQDDJRQTB", "NZXUD", "IHUFVVHGWQEM", "HCTGMLPNOVB"]
 def question06(portfolio):
     # modify and then return the variable below
-    answer = -5
-    return answer
+#    port = np.asarray(portfolio)
+#    letter = [(i[0],i[-1]) for i in port]
+#    
+#    unique_elements, unique_inverse, unique_counts = np.unique(letter, axis=0, return_counts=True, return_inverse=True)
+    
+    
+    return 5
