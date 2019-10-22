ed1e9c9bdb7788859cbd2dfed2df1750297a1871
RoundofThree <40538341+RoundofThree@users.noreply.github.com>
2019-10-22 23:59:02 +0100

    Update Question2.java

--- java/src/main/java/skeleton/answers/Question2.java
+++ java/src/main/java/skeleton/answers/Question2.java
@@ -3,7 +3,16 @@ package skeleton.answers;
 public class Question2 {
 
     public static int riskAndReward(int[] risk, int[] bonus, int[] trader) {
-        // TODO Auto-generated method stub
-        return -5;
+        int teamMaxScore = 0;
+        for(int td : trader) {
+            int max = 0;
+            for(int i = 0; i < bonus.length; i++){
+                if(td >= risk[i] && bonus[i] > max){
+                    max = bonus;
+                }
+            }
+            teamMaxScore += max;
+        } 
+        return teamMaxScore;
     }
 }
