36af7ffa651980697cdebb0e6dde565931653d90
RoundofThree <40538341+RoundofThree@users.noreply.github.com>
2019-10-23 00:00:58 +0100

    Update Question1.java

--- java/src/main/java/skeleton/answers/Question1.java
+++ java/src/main/java/skeleton/answers/Question1.java
@@ -3,8 +3,23 @@ package skeleton.answers;
 public class Question1 {
 
     public static double calculateTotalPayment(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage) {
-        // TODO Auto-generated method stub
-        return -5;
+        double currentDebt = initialLevelOfDebt;
+        double fixedRepayment = ((repaymentPercentage * initialLevelOfDebt / 100) > 50)? (repaymentPercentage * initialLevelOfDebt / 100) : 50;
+        double deposit = 0.10 * initialLevelOfDebt;
+        double totalAmountSpent = 0;
+        
+        
+        while(currentDebt > 0) {
+            currentDebt = (1 + interestPercentage / 100) * currentDebt;
+            if(currentDebt < fixedRepayment) {
+                currentDebt = 0;
+                totalAmountSpent += currentDebt;
+            } else {
+            currentDebt -= fixedRepayment;
+            totalAmountSpent += fixedRepayment;
+            }      
+        }
+        return totalAmountSpent + deposit;
     }
 
 }
