783c628211b0c6a65d9b143fdd6e18da3932e0eb
RoundofThree <40538341+RoundofThree@users.noreply.github.com>
2019-10-23 01:01:18 +0100

    Update Question6.java

--- java/src/main/java/skeleton/answers/Question6.java
+++ java/src/main/java/skeleton/answers/Question6.java
@@ -2,8 +2,20 @@ package skeleton.answers;
 
 public class Question6 {
 
-    public static int startingAndEndingWell(String[] portfolio) {
-        // TODO Auto-generated method stub
-        return -5;
+    public static int startingAndEndingWell(String[] portfolio, String starting, String ending) {
+        int highestValue = 0;
+        boolean found = false;
+        for(i = 0; i < portfolio.length; i++){
+            if(portfolio[i].startsWith(starting) && portfolio[i].endsWith(ending)){
+                found = true;
+                highestValue = i;
+            }
+        }
+        
+        if(!found){
+            return -1;
+        } else {
+            return highestValue;
+        }
     }
 }
